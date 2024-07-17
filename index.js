c904757679815b79c7125c49d38cda4425ab88e0
Akshay Navale <169084830+akshay20n@users.noreply.github.com>
2024-07-17 14:04:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-$(document).ready(function() {
-    $('#generate-joke').click(function() {
-        $.ajax({
-            url: 'https://icanhazdadjoke.com/',
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json'
-            },
-            success: function(data) {
-                $('.joke').text(data.joke);
-            },
-            error: function() {
-                $('.joke').text('Oops! Something went wrong. Try again later.');
-            }
+const dadJokeButton = document.querySelector("#generate-dad-joke");
+
+dadJokeButton.addEventListener("click", () => {
+    fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementsByClassName("joke")[0].textContent = data.joke;
+            console.log(data.joke);
+        })
+        .catch(error => {
+            console.error("There is a problem with API !!!", error);
+            document.getElementsByClassName("joke")[0].textContent = "Oops! Something went wrong. Try again later.";
         });
-    });
+
+    setTimeout(() => {
+        const audio = new Audio("hahasound.mp3");
+        audio.play();
+    }, 500);
 });
